d71aa23bb96f4676741e686e982ed805d64a158d
hoangcaominh <hoangcaominh1812@gmail.com>
2023-11-13 18:26:14 +0700

    Update tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,9 +4,28 @@ export default {
   content: [
     "./index.html",
     "./src/**/*.{vue,js,ts,jsx,tsx}",
+    "./node_modules/primevue/**/*.{vue,js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px',
+    },
+    fontFamily: {
+      sans: ['Graphik', 'sans-serif'],
+      serif: ['Merriweather', 'serif'],
+    },
+    extend: {
+      spacing: {
+        '128': '32rem',
+        '144': '36rem',
+      },
+      borderRadius: {
+        '4xl': '2rem',
+      }
+    }
   },
   plugins: [],
 } satisfies Config
