4257b695d05729fbaaf19d88bbab7f1ada74882b
hoangcaominh <hoangcaominh1812@gmail.com>
2023-11-13 18:26:35 +0700

    Add Primevue stuffs

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,7 @@
 import { createApp } from "vue"
 import { createPinia } from "pinia"
 
-// PrimeVue stuffs
-import PrimeVue from "primevue/config"
-import ToastService from "primevue/toastservice"
+import "./assets/main.css"
 
 
 import App from "./App.vue"
@@ -15,7 +13,16 @@ app.use(createPinia())
 app.use(router)
 
 // PrimeVue stuffs
+import 'primevue/resources/themes/mdc-light-indigo/theme.css'
+import 'primeicons/primeicons.css'
+
+import PrimeVue from "primevue/config"
 app.use(PrimeVue)
+
+import ToastService from "primevue/toastservice"
 app.use(ToastService)
+
+import Toast from "primevue/toast"
+app.component("Toast", Toast)
 
 app.mount("#app")
