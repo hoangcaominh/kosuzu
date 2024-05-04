0b0b6f110a1c278ddeb205f4da4ad12ac78602f4
hoangcaominh <hoangcaominh1812@gmail.com>
2024-05-04 23:18:33 +0700

    Add EoSD stages

--- src/locales/en/index.ts
+++ src/locales/en/index.ts
@@ -142,6 +142,15 @@ export default {
                     "Stage practice",
                     "Spell practice",
                     "Versus",
+                ],
+                stage: [
+                    "1",
+                    "2",
+                    "3",
+                    "4",
+                    "5",
+                    "6",
+                    "Extra"
                 ]
             },
         },
