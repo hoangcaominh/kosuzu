bbf5278bedbadf6417119d7d857d67f2da8b4fa9
hoangcaominh <hoangcaominh1812@gmail.com>
2024-05-04 23:52:16 +0700

    Fix GFW route name

--- src/locales/en/index.ts
+++ src/locales/en/index.ts
@@ -104,12 +104,12 @@ export default {
                 th128: {
                     title: "Great Fairy Wars",
                     route: [
-                        "A-1",
-                        "A-2",
-                        "B-1",
-                        "B-2",
-                        "C-1",
-                        "C-2",
+                        "A1",
+                        "A2",
+                        "B1",
+                        "B2",
+                        "C1",
+                        "C2",
                         "Ex"
                     ],
                     stage: [
