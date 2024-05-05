0a6be7ef20bb62aded54653a0f54160ea8740768
hoangcaominh <hoangcaominh1812@gmail.com>
2024-05-05 22:38:10 +0700

    TD: Add more features to stage table

--- src/components/tables/Th13Summary.vue
+++ src/components/tables/Th13Summary.vue
@@ -6,6 +6,7 @@ import { CONSTANTS } from "thrpy-parser"
 defineProps(["filename", "info"])
 
 const stagesActive = ref(false)
+const EXTEND_THRESHOLD = [8, 10, 12, 15, 18, 20, 25]
 </script>
 
 <template>
@@ -87,7 +88,6 @@ const stagesActive = ref(false)
         </tr>
       </tbody>
     </table>
-    <!-- TODO: Add more features to each stage -->
     <Transition name="stages-slide">
       <table
         class="container border border-separate border-slate-500 text-left shadow-lg shadow-th13/50"
@@ -101,11 +101,27 @@ const stagesActive = ref(false)
           </tr>
         </thead>
         <tbody>
+          <tr>
+            <th class="border border-slate-500 px-3 py-1">Stage</th>
+            <th class="border border-slate-500 px-3 py-1">Score</th>
+            <th class="border border-slate-500 px-3 py-1">Lives</th>
+            <th class="border border-slate-500 px-3 py-1">Bombs</th>
+            <th class="border border-slate-500 px-3 py-1">Power</th>
+            <th class="border border-slate-500 px-3 py-1">PIV</th>
+            <th class="border border-slate-500 px-3 py-1">Graze</th>
+            <th class="border border-slate-500 px-3 py-1">Trance</th>
+          </tr>
           <tr v-for="stage in info.stages" :key="stage.stage">
-            <th class="border border-slate-500 px-3 py-1">
-              Stage {{ $t(`table.values.stage[${stage.stage}]`) }}
-            </th>
+            <th class="border border-slate-500 px-3 py-1">{{ $t(`table.labels.stage_number`, { stage: stage.stage }) }}</th>
             <td class="border border-slate-500 px-3 py-1">{{ $n(stage.score) }}</td>
+            <td class="border border-slate-500 px-3 py-1">
+              {{ (!isNaN(stage.lives)) ? `${stage.lives} (${stage.life_pieces}/${EXTEND_THRESHOLD[Math.min(stage.extends, EXTEND_THRESHOLD.length)]})` : "" }}
+            </td>
+            <td class="border border-slate-500 px-3 py-1">{{ (!isNaN(stage.bombs)) ? `${stage.bombs} (${stage.bomb_pieces}/8)` : "" }}</td>
+            <td class="border border-slate-500 px-3 py-1">{{ (!isNaN(stage.power)) ? (stage.power / 100).toFixed(2) : "" }}</td>
+            <td class="border border-slate-500 px-3 py-1">{{ stage.piv }}</td>
+            <td class="border border-slate-500 px-3 py-1">{{ stage.graze }}</td>
+            <td class="border border-slate-500 px-3 py-1">{{ (!isNaN(stage.trance)) ? (stage.trance / 200).toFixed(2) : "" }}</td>
           </tr>
         </tbody>
       </table>
