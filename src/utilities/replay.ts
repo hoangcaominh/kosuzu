c13c9fd61c862f20ec39d31f154289b1e811a06b
hoangcaominh <hoangcaominh1812@gmail.com>
2024-05-05 22:15:27 +0700

    Remove switch case for appendStartingStage
    Turns out my parser design is so smart (for now)

--- src/utilities/replay.ts
+++ src/utilities/replay.ts
@@ -1,24 +1,11 @@
-import { CONSTANTS } from "thrpy-parser"
-
 export function appendStartingStage(replay: any): object {
-    switch (replay.game) {
-        case CONSTANTS.GAME.TH06.ID:
-        case CONSTANTS.GAME.TH07.ID:
-        case CONSTANTS.GAME.TH08.ID:
-        case CONSTANTS.GAME.TH10.ID:
-        case CONSTANTS.GAME.TH11.ID: {
-            let newIdx = -1
-            for (const stageInstance of replay.stages)
-                [stageInstance.stage, newIdx] = [newIdx, stageInstance.stage]
+    let newIdx = -1
+    for (const stageInstance of replay.stages)
+        [stageInstance.stage, newIdx] = [newIdx, stageInstance.stage]
 
-            replay.stages.push({
-                stage: newIdx,
-                score: replay.score
-            })
-            break
-        }
-        default:
-            break
-    }
+    replay.stages.push({
+        stage: newIdx,
+        score: replay.score
+    })
     return replay
 }
