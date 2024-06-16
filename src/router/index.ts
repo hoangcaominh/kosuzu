455fd1a953a8b66702e51422bf6ea37124634f8f
hoangcaominh <hoangcaominh1812@gmail.com>
2024-06-16 12:21:46 +0700

    Revert "Fix router path"
    This reverts commit a6859eeedc7698ac3ab72fa66c3f780da4347896.

--- src/router/index.ts
+++ src/router/index.ts
@@ -4,7 +4,7 @@ import HomeView from "@/views/HomeView.vue"
 import AboutView from "@/views/AboutView.vue"
 
 const router = createRouter({
-  history: createWebHistory("/kosuzu/"),
+  history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: "/",
